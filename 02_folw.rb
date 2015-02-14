e94300899735eb5bf8183b75c9ede4eaf197b4ef
sTinGe <stinge62580@gmail.com>
2015-02-15 00:19:06 +0800

    Add some flow control example

--- 02_folw.rb
+++ 02_folw.rb
@@ -1,5 +1,7 @@
+# if-elsif-else statement
 print "Integer please:"
 user_num = Integer(gets.chomp)
+# user_num = gets.to_i
 
 if user_num < 0
   puts "You picked a negative integer!"
@@ -8,3 +10,26 @@ elsif user_num > 0
 else
   puts "You picked zero!"
 end
+
+# case statement
+print "Enter domain:"
+domain = gets.chomp
+domain.downcase
+case domain
+when "us"
+  puts "United States"
+when "uk"
+  puts "United Kingdom"
+else
+  puts "Unknown"
+end
+
+# while statement
+i = 0
+loop = 0
+while i < 10 do
+  i = i+1
+  loop = loop+i 
+end
+
+puts "The sum of 0..9 is #{loop}"
